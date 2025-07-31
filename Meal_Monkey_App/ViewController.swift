a875478690fda07e6588ea848c562e46aef74df1
akshaynena8461 <akshay.nena@brainvire.com>
2025-07-31 17:05:24 +0530

    Initial Commit

---
+++ Meal_Monkey_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meal_Monkey_App
+//
+//  Created by Akshay Nena on 31/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
