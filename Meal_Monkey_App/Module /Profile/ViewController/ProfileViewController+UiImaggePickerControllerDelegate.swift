659beb6f942b8de94af833fefdc1fea24a173ef2
akshaynena8461 <akshay.nena@brainvire.com>
2025-08-06 13:24:28 +0530

    Added Edit Profile Page

---
+++ Meal_Monkey_App/Module /Profile/ViewController/ProfileViewController+UiImaggePickerControllerDelegate.swift
@@ -0,0 +1,9 @@
+
+import UIKit
+
+extension ProfileViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imgProfile.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        dismiss(animated: true)
+    }
+}
---
+++ Meal_Monkey_App/Module /Profile/ViewController/ProfileViewController.swift
@@ -0,0 +1,49 @@
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBAction func btnEditProfileClick(_ sender: Any) {
+    }
+    @IBAction func btnSaveClick(_ sender: Any) {
+    }
+    @IBAction func btnSignOutClick(_ sender: Any) {
+    }
+    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtMobile: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var stackDetail: UIStackView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btnEditProfile: UIButton!
+    @IBOutlet weak var btnSignOut: UIButton!
+    @IBOutlet weak var imgProfile: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imgProfile.layer.cornerRadius = imgProfile.frame.size.width / 2
+        imgProfile.layer.masksToBounds = true
+        imgProfile.layer.borderWidth = 2
+        imgProfile.layer.borderColor = UIColor.systemGray.cgColor
+        
+        let imageGesture = UITapGestureRecognizer(target: self, action: #selector(addImage))
+        imgProfile.addGestureRecognizer(imageGesture)
+        
+        setLeftAlignedTitle("Profile")
+        setCartButton(target: self, action: #selector(cartBtnTapped))
+        
+        EditStyle.setborder(textfields: [txtName,txtEmail,txtMobile,txtAddress,btnSave])
+        EditStyle.setPadding(textFields: [txtName,txtEmail,txtMobile,txtAddress], paddingWidth: 34)
+
+    }
+    @objc func addImage() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.allowsEditing = true
+        present(pickerController, animated: true)
+    }
+    @objc func cartBtnTapped(){
+        print("Cart Btn Tapped")
+    }
+}
