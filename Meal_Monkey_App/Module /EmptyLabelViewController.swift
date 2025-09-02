dc733af28b430aba57ada75e3220082a8db66fd5
akshaynena8461 <akshay.nena@brainvire.com>
2025-09-02 14:59:39 +0530

    Added PodFile and have done setup then add activity indicator and otp feature using pod libraries

---
+++ Meal_Monkey_App/Module /EmptyLabelViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmptyLabelViewController.swift
+//  Meal_Monkey_App
+//
+//  Created by Akshay Nena on 29/08/25.
+//
+
+import UIKit
+
+class EmptyLabelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
